672c711a11a89c9983771aa4d3e0ba20eabe6aa8
Radmir <yaradmir2203@gmail.com>
2022-05-24 23:04:50 +0500

    +1 println

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("HI!!!");
+        System.out.println("HI!!1111");
+        System.out.println("Hello");
+        System.out.println("Hello");
     }
 
 }
