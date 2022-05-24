bcd595f27f4dd02546196e18c2cc1f4f68319492
Radmir <yaradmir2203@gmail.com>
2022-05-24 23:25:40 +0500

    Hi friend

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("HI!!1111");
+        System.out.println("Hello world"+"Hi friend");
         System.out.println("Hello");
         System.out.println("Hello");
         System.out.println("Hello");
